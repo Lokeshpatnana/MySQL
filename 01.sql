91a5378856fe9134e0fe202d1b1cb2f1dee58b7a
PATNANA LOKESWARA RAO <109509575+Lokeshpatnana@users.noreply.github.com>
2022-09-13 10:51:28 +0530

    Add files via upload

---
+++ 01.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Lokesh 
+(
+    CustomerID	varchar(300),
+    CustomerName	varchar(300),
+    ContactName	varchar(300),
+    Address	varchar(300),
+    City	varchar(300),
+    PostalCode	varchar(300),
+    Country	varchar(300)
+);
+
+INSERT INTO Lokesh (CustomerID,CustomerName,ContactName,Address,City,PostalCode,Country) VALUES ('1', 'Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany');
+INSERT INTO Lokesh (CustomerID,CustomerName,ContactName,Address,City,PostalCode,Country) VALUES ('2', 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Avda. de la Constitución 2222', 'México D.F.', '05021', 'Mexico');
+INSERT INTO Lokesh (CustomerID,CustomerName,ContactName,Address,City,PostalCode,Country) VALUES ('3', 'Antonio Moreno Taquería', 'Antonio Moreno', 'Mataderos 2312', 'México D.F.', '05023', 'Mexico');
+INSERT INTO Lokesh (CustomerID,CustomerName,ContactName,Address,City,PostalCode,Country) VALUES ('4', 'Around the Horn', 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK');
+INSERT INTO Lokesh (CustomerID,CustomerName,ContactName,Address,City,PostalCode,Country) VALUES ('5', 'Berglunds snabbköp', 'Christina Berglund', 'Berguvsvägen 8', 'Luleå', 'S-958 22', 'Sweden');
